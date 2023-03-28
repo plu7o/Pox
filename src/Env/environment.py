4b3583d6d14011580481d433599a6fd67f3bc983
plu7o <skankhunt4orty2wo@gmail.com>
2023-03-29 01:15:56 +0200

    Pox V0.3.6.3 - added scope to Environment class and respective methods  Changes to be committed: 	new file:   src/Env/__init__.py 	modified:   src/Env/environment.py

--- src/Env/environment.py
+++ src/Env/environment.py
@@ -1,13 +1,17 @@
 from Errors.runtime_error import Runtime_error
 from Lexer.token import Token
 
+
 class Environment:
-    def __init__(self) -> None:
+    def __init__(self, enclosing: Environment = None) -> None:
+        self.enclosing = enclosing
         self.values = {}
 
     def get(self, name: Token) -> object:
         if name.lexeme in self.values:
             return self.values[name.lexeme]
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
 
         raise Runtime_error(name, f'Undefined variable "{name.lexeme}".')
 
@@ -15,12 +19,11 @@ class Environment:
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
             return
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
+            return
 
         raise Runtime_error(name, f'Undefined variable "{name.lexeme}".')
 
-
     def define(self, name: str, value: object):
         self.values[name] = value
-
-
-
